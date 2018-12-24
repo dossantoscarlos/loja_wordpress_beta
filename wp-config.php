13267967b140986040e52b846d36bf945ce44eb9
dossantoscarlos <anjoazrael10@gmail.com>
2018-12-24 00:20:54 -0200

    correcao de conexao

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'b598d187b9b5cd');
 define('DB_PASSWORD', 'a4f420b1');
 
 /** MySQL hostname */
-define('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net');
+define('DB_HOST', 'https://us-cdbr-iron-east-01.cleardb.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
